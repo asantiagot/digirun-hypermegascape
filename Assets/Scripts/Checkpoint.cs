31bfa190fdd3cd78e6725e779483da053933644a
Antonio Santiago <an.santiagot@gmail.com>
2024-05-31 09:56:04 -0600

    Add checkpoints in first sections

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -3,6 +3,15 @@ using UnityEngine;
 public class Checkpoint : MonoBehaviour
 {
     public GameObject restartFromCheckpointButton;
+    
+    // Commenting this code, this rotates the Cassette Model, but it's not working properly
+    // public float rotationSpeed = 50f;
+
+    // void Update()
+    // {
+    //     transform.Rotate(0, rotationSpeed * Time.deltaTime, 0, Space.Self);
+    // }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
